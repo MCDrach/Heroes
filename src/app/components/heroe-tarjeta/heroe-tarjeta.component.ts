d72c3df81bb24dc7a95c99f67a42d17af38d9999
Darwin Aguilar <darwinchilon@gmail.com>
2020-01-16 04:25:35 +0100

    modificacion de omponente

---
+++ src/app/components/heroe-tarjeta/heroe-tarjeta.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Heroe } from '../../models/Heroe';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-heroe-tarjeta',
+  templateUrl: './heroe-tarjeta.component.html',
+  styles: []
+})
+export class HeroeTarjetaComponent implements OnInit {
+
+  @Input() heroe: Heroe = new Heroe();
+  @Input() index: number;
+  @Output() heroeSeleccionado = new EventEmitter<number>();
+
+  constructor(
+    private router: Router
+  ) { }
+
+  ngOnInit() {
+  }
+
+  verHeroe(){
+    // console.log('index',this.index);
+    // this.router.navigate(['heroe', this.index]);
+  }
+
+}
